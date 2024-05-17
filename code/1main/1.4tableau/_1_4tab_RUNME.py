4ebb88cc7d2e654417e4d248faedefceebcac0b5
Nathan Kuhn <nathankuhn@ku.edu>
2024-05-17 11:53:00 -0500

    all: consolidating key values.

--- code/1main/1.4tableau/_1_4tab_RUNME.py
+++ code/1main/1.4tableau/_1_4tab_RUNME.py
@@ -95,59 +95,7 @@ print('Local Code Repository: ', str(*[path for path in Path.cwd().parents if pa
 #%%
 import sys
 sys.path.append(str(*[path for path in Path.cwd().parents if path.name == 'nehv_ds_code_repository']))
-from packages_and_functions import * 
-
-
-#%%##################################################
-### SETTINGS ###
-#####################################################
-
-
-#%%##################################################
-### KEY VALUES ###
-#####################################################
-
-# str_nehv_quarter = 'Y12Q1 (Oct 2022 - Dec 2023)'
-# str_nehv_quarter = 'Y12Q2 (Oct 2022 - Mar 2023)'
-# str_nehv_quarter = 'Y12Q3 (Oct 2022 - Jun 2023)'
-# str_nehv_quarter = 'Y12Q4 (Oct 2022 - Sep 2023)'
-#str_nehv_quarter = 'Y13Q1 (Oct 2023 - Dec 2023)'
-str_nehv_quarter = 'Y13Q2 (Oct 2023 - Mar 2024)'
-
-int_nehv_year = 13 
-
-# int_nehv_quarter = 1 
-int_nehv_quarter = 2 
-
-# # date_fy_start = pd.Timestamp("2022-10-01")
-# # date_fy_end = pd.Timestamp("2023-09-30")
-# date_fy_start = pd.Timestamp("2023-10-01")
-# date_fy_end = pd.Timestamp("2024-09-30")
-
-bool_14t_deduplicate = True  
-
-#%%########################
-### Federal Poverty Guidelines
-    ### https://aspe.hhs.gov/topics/poverty-economic-mobility/poverty-guidelines 
-    ### https://aspe.hhs.gov/topics/poverty-economic-mobility/poverty-guidelines/prior-hhs-poverty-guidelines-federal-register-references 
-
-### 2022: https://www.federalregister.gov/documents/2022/01/21/2022-01166/annual-update-of-the-hhs-poverty-guidelines 
-if (str_nehv_quarter in ('Y12Q1 (Oct 2022 - Dec 2023)')):
-    ### 8870 + (4720 * [household size])
-    int_fpg_base = 8870 
-    int_fpg_increment = 4720 
-
-### 2023: https://www.federalregister.gov/documents/2023/01/19/2023-00885/annual-update-of-the-hhs-poverty-guidelines 
-elif (str_nehv_quarter in ('Y12Q2 (Oct 2022 - Mar 2023)', 'Y12Q3 (Oct 2022 - Jun 2023)', 'Y12Q4 (Oct 2022 - Sep 2023)', 'Y13Q1 (Oct 2023 - Dec 2023)')):
-    ### 9440 + (5140 * [household size])
-    int_fpg_base = 9440 
-    int_fpg_increment = 5140 
-
-### 2024: https://www.govinfo.gov/content/pkg/FR-2024-01-17/pdf/2024-00796.pdf 
-elif (str_nehv_quarter in ('Y13Q2 (Oct 2023 - Mar 2024)')):
-    ### 9680 + (5380 * [household size])
-    int_fpg_base = 9680 
-    int_fpg_increment = 5380 
+from RUNME import * 
 
 
 #%%##################################################
