57e5ec27b7a5933d8f4c3cc08f0cfa5f85781d31
Eric Myers <egmyers@ku.edu>
2024-12-11 10:59:16 -0600

    2.2: now have a matching 'CPS Master File'

--- code/2CFS/2.2fromCFS/2.2fromCFS.py
+++ code/2CFS/2.2fromCFS/2.2fromCFS.py
@@ -33,6 +33,8 @@ path_22_dir_output = Path(path_22_files_base, '9out', str_nehv_quarter)
 
 ##
 path_22_input_CFS_file = Path(path_22_dir_input, 'MIECHV Report - Child Y13Q4.xls')
+path_22_input_child_file = Path(path_22_dir_input, 'Child Activity Master File.xlsx')
+path_22_input_master_file = Path(path_22_dir_input, 'Child CPS Master File.xlsx')
 
 
 with xw.App(visible=True) as app:  # Keep Excel hidden while running
@@ -49,6 +51,9 @@ if isinstance(data, list) and len(data) > 0:
     df_22_CFS_file = pd.DataFrame(data[1:], columns=data[0])
 
 #list_path_21_input_raw_sheets=['LLCHD','FamilyWise', 'Project ID']
+df_22_child_LL = pd.read_excel(path_22_input_child_file, sheet_name='LLCHD', keep_default_na=False)
+df_22_child_FW = pd.read_excel(path_22_input_child_file, sheet_name='Family Wise', keep_default_na=False)
+df_22_child_pID = pd.read_excel(path_22_input_child_file, sheet_name='Project ID', keep_default_na=False)# dtype=dict_12LL_col_dtypes_1)
 
 # %% ################################################
 ### Clean File ###
@@ -78,6 +83,7 @@ df_22_CFS_file = df_22_CFS_file[df_22_CFS_file['Current Status Reason'].isin(val
 
 ##3. Delete all records without a Status 
 df_22_CFS_file = df_22_CFS_file.dropna(subset=['Current Status Reason'])
+df_22_CFS_file.rename(columns={'Current Status Reason': 'Status'}, inplace=True)
 
 ##4. Remove duplicates across Project ID and Intake Received Date (oldest to newest)
 df_22_CFS_file['Intake Received'] = pd.to_datetime(df_22_CFS_file['Intake Received'], errors='coerce')
@@ -86,10 +92,12 @@ df_22_CFS_file = df_22_CFS_file.drop_duplicates(subset=['Intake Received', 'Proj
 
 # Sort the DataFrame by 'Intake Received Date' (oldest to newest)
 df_22_CFS_file = df_22_CFS_file.sort_values(by='Intake Received', ascending=True)
+df_22_CFS_file.rename(columns={'Intake Received': 'IntakeReceivedDate'}, inplace=True)
+df_22_CFS_file['IntakeReceivedDate'] = df_22_CFS_file['IntakeReceivedDate'].dt.strftime('%m/%d/%Y')
 
 
 ### check that this matches Joe's v2 file --it does!! for Y13Q4
-##df_22_CFS_file.to_csv(Path(path_22_dir_output, 'CPS File v2.csv'), index = False, date_format="%m/%d/%Y")
+df_22_CFS_file.to_csv(Path(path_22_dir_output, 'CPS File v2.csv'), index = False, date_format="%m/%d/%Y")
 
 #%%##############################################!>>>
 ### >>> RESTRUCTURING  
@@ -104,29 +112,89 @@ df_22_CFS_file = df_22_CFS_file.sort_values(by='Intake Received', ascending=True
 ##after that restructure, and cross-check against Joe's MIECHV report v5
 
 
+## Create the columns for v5
+df_22_CFS_file['year']=int_nehv_year
+df_22_CFS_file['quarter']=int_nehv_quarter
+df_22_CFS_file['agency_code'] = df_22_CFS_file['Project Id'].str[:2]
+df_22_CFS_file['family_id'] = df_22_CFS_file['Project Id'].str[2:].str.split('-').str[0]
+df_22_CFS_file['tgt_id'] = df_22_CFS_file['Project Id'].str.split('-').str[1]
+
+print(df_22_CFS_file)
+
+
 
 ### Pivot the DataFrame:
-df_22_CFS_file = df_22_CFS_file.pivot_table(
-    index=['Project Id', 'year', 'quarter', 'agency_code','family_id', 'tgt_id','funding','Has_ChildWelfareAdaptation','Adaptation'] ### All columns that do not change (if not listed will be deleted).
+df_22_CFS_file['group_count'] = df_22_CFS_file.groupby(['Project Id']).cumcount() + 1
+
+df_22_CFS_file_pivoted = df_22_CFS_file.pivot_table(
+    index=['Project Id', 'year', 'quarter', 'agency_code','family_id','tgt_id']  ### All columns that do not change (if not listed will be deleted).
     ,columns=df_22_CFS_file.groupby(['Project Id']).cumcount() + 1 ### Cumulative count of rows within groupings so groups of data stack vertically. DF should be sorted beforehand. 
-    ,values=['Finding', 'Intake Recieved', 'Current Status Reason'] ### Columns that change.
+    ,values=['Finding', 'IntakeReceivedDate', 'Status'] ### Columns that change.
     ,aggfunc='first' ### To use the values themselves and not an aggregation.
 )
-### TODO ASKJOE: Do we need col 'ERVisitReason'? Joe does not keep.
 
 #%%
 ### Reorder exploded columns (while all other columns still in the row index & while column names still a MultiIndex):
-df_22_CFS_file = df_22_CFS_file.sort_index(axis=1, level=0, ascending=False).sort_index(axis=1, level=1, sort_remaining=False) 
+df_22_CFS_file = df_22_CFS_file_pivoted.sort_index(axis=1, level=0, ascending=False).sort_index(axis=1, level=1, sort_remaining=False)
 
-#%%
-### Flatten the column MultiIndex & rename columns in the style of SPSS:
-df_22_CFS_file.columns = [f'{col[0]}.{col[1]}' for col in df_12LL_pivoted_ChildERInj_2.columns]
+# Flatten the MultiIndex columns and rename in the style of SPSS (e.g., 'Finding.1', 'Intake Received.2')
+df_22_CFS_file.columns = [f'{col[0]}.{col[1]}' for col in df_22_CFS_file.columns]
 
-#%%
-### Reset row & column indices:
 df_22_CFS_file = df_22_CFS_file.reset_index()
 
+df_22_CFS_file.drop_duplicates(inplace=True)
 
+#%%##############################################!>>>
+### >>> VLOOKUPS  
+#####################################################
+##Bring in funding, CWA, and Adapation columns from Child Activity File
+# Merge the DataFrames based on the common column ('Project ID')
+
+# Rename columns to ensure consistency in column names
+df_22_CFS_file.rename(columns={'Project Id': 'project_id'}, inplace=True)
+#df_22_child_LL.rename(columns={'project_id': 'Project ID'}, inplace=True)
+
+# Check column names to verify the rename
+print(df_22_CFS_file.columns)
+print(df_22_child_LL.columns)
+
+
+# Perform the first merge with df_22_child_LL
+# df_22_LL_columns = df_22_child_LL[['Project ID', 'funding', 'Has_ChildWelfareAdaptation']]
+# df_22_CFS_file = pd.merge(df_22_CFS_file, df_22_LL_columns, on=['Project ID'], how='left')
+
+# #Perform the second merge with df_22_child_FW
+# df_22_FW_columns = df_22_child_FW[['Project ID', 'Adaptation', ]]
+# df_22_CFS_file = pd.merge(df_22_CFS_file, df_22_FW_columns, on=['Project ID'], how='left')
+
+##for some reason when I bring over these columns more rows are added, (I'm assuming because of more unique column values)
+# After the merge, check for NaN values in the new columns
+
+df_22_CFS_file['Adaptation']=None
+df_22_CFS_file['Has_ChildWelfareAdaptation']= None
+df_22_CFS_file['funding']=None
+
+## Check that output matches Joe's v5
+df_22_CFS_file.to_csv(Path(path_22_dir_output, 'CPS File v5.csv'), index = False, date_format="%m/%d/%Y")
+
+
+### 6. Pull from existing file and append new quarter to old file, write new combined to output location for CPS Master File
+
+if int_nehv_quarter != 1:
+    df_22_CPS_previous = pd.read_excel(path_22_input_master_file, sheet_name='CPS Data', keep_default_na=False, na_values=[''])
+    df_22_CPS_master = pd.concat([df_22_CPS_previous, df_22_CFS_file], ignore_index=True)
+else:
+    df_22_CPS_master=df_22_CFS_file
+
+
+with pd.ExcelWriter(Path(path_22_dir_output, 'Child CPS Master File auto.xlsx'), engine='openpyxl') as writer:
+        df_22_CPS_master.to_excel(writer, index=False, sheet_name='CPS Data')
+        df_22_child_FW .to_excel(writer, index=False, sheet_name='Family Wise')
+        df_22_child_LL.to_excel(writer, index=False, sheet_name='LLCHD')
+        df_22_child_pID.to_excel(writer, index=False, sheet_name='Project ID')
+
+#the output of this version currently matches what Joe has for Q4 file for CPS Master File
+#this works! touch in with Joe about Zip? Not sure where it's coming from if it is needed 
 
 
 
